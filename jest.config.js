797e9d0193819cc7e04ea345933eb51ae5dbb73d
Artem <artemmail@gmail.com>
2024-11-30 10:24:23 +0400

    debug

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,12 @@
 module.exports = {
     testEnvironment: "jsdom",
-    roots: ["<rootDir>/src"],
-    testMatch: ["**/*.{js,jsx,ts,tsx}"],
-    moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
     },
-    transformIgnorePatterns: [
-        "/node_modules/(?!(axios)/)"
-    ],
+    transformIgnorePatterns: ["/node_modules/(?!(axios)/)"],
     moduleNameMapper: {
-        "\\.(css|scss|sass)$": "identity-obj-proxy",
+      "\\.(css|scss|sass)$": "identity-obj-proxy"
     },
-    extensionsToTreatAsEsm: [".jsx", ".js"],
-};
+    extensionsToTreatAsEsm: [".jsx", ".js"]
+  };
+  
